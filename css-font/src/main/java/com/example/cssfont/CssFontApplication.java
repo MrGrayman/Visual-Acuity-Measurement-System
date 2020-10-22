d2823e3007122e36e5a8f0f12f159e1dcdfc733c
time1941 <time254133@gmail.com>
2020-10-22 11:11:51 +0700

    css-font

---
+++ css-font/src/main/java/com/example/cssfont/CssFontApplication.java
@@ -0,0 +1,13 @@
+package com.example.cssfont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssFontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssFontApplication.class, args);
+	}
+
+}
---
+++ css-font/src/test/java/com/example/cssfont/CssFontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cssfont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssFontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
