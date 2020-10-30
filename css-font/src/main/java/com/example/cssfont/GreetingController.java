e54eb91b42f9eada2e90c0bdde897a153a3b2b1c
time1941 <time254133@gmail.com>
2020-10-30 11:52:48 +0700

    font 2 from dev gun & calculator

--- css-font/src/main/java/com/example/cssfont/GreetingController.java
+++ css-font/src/main/java/com/example/cssfont/GreetingController.java
@@ -8,10 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/font1")
+    public String font1(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "font1";
+    }
+    @GetMapping("/font2")
+    public String font2(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "font2";
+    }
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "calculator";
     }
 
 }
